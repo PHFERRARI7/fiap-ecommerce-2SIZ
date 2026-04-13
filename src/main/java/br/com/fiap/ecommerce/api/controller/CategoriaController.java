9d7aa9d5b7709239adfb407c1a70e87bd9a5cd24
PHFERRARI7 <phferrari7@gmail.com>
2026-04-13 20:54:33 -0300

    Atualização da aula 1 dia 13/04/2026

--- src/main/java/br/com/fiap/ecommerce/api/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/ecommerce/api/controller/CategoriaController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,34 +26,47 @@ public class CategoriaController {
     @Transactional //rollback no banco em caso de erros
     @PostMapping //recebe as requisições do tipo POST
     //Recebe o conteúdo de Body e garante que é válido conforme as retrições do DTO
-    public void cadastrarCategoria(@RequestBody @Valid DadosCadastroCategoria dados) {
-        categoriaRepository.save(new Categoria(dados));
+    public ResponseEntity<DadosDetalhamentoCategoria> cadastrarCategoria(@RequestBody @Valid DadosCadastroCategoria dados, UriComponentsBuilder uriBuilder) {
+        var categoria = new Categoria(dados);
+        categoriaRepository.save(categoria);
+
+        var uri = uriBuilder.path("/categorias/{id}").buildAndExpand(categoria.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoCategoria(categoria));
     }
 
     @GetMapping //recebe as requisições do tipo GET
-    public Page<DadosListagemCategoria> listarCategorias(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return categoriaRepository.findAllByAtivoTrue(paginacao)
+    public ResponseEntity<Page<DadosListagemCategoria>> listarCategorias(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = categoriaRepository.findAllByAtivoTrue(paginacao)
                 .map(DadosListagemCategoria::new);
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public DadosDetalhamentoCategoria buscarPorId(@PathVariable Long id) {
-        var categoria = categoriaRepository.getReferenceById(id);
-        return new DadosDetalhamentoCategoria(categoria);
+    public ResponseEntity buscarPorId(@PathVariable Long id) {
+        Categoria categoria = categoriaRepository.findByIdAndAtivoTrue(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Categoria não existe"
+                ));
+        return ResponseEntity.ok(new DadosDetalhamentoCategoria(categoria));
     }
 
     @PutMapping
     @Transactional
-    public void atualizarCategoria(@RequestBody @Valid DadosAtualizarCategoria dados) {
+    public ResponseEntity atualizarCategoria(@RequestBody @Valid DadosAtualizarCategoria dados) {
         var categoria = categoriaRepository.getReferenceById(dados.id());
         categoria.atualizarCategoria(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoCategoria(categoria));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletarCategoria(@PathVariable Long id) {
+    public ResponseEntity deletarCategoria(@PathVariable Long id) {
         // categoriaRepository.deleteById(id);    DELETA DE VERDADE
         var categoria = categoriaRepository.getReferenceById(id);
         categoria.excluirCategoria();
+
+        return ResponseEntity.noContent().build();
     }
 }
